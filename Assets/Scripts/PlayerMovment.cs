ca6e75f3192ed6d74229c54048c2b40d9e460e8b
aleksanderszturo <aleksanderszturo@gmail.com>
2023-01-05 15:37:06 +0100

    Fliping Player when moving left right

--- Assets/Scripts/PlayerMovment.cs
+++ Assets/Scripts/PlayerMovment.cs
@@ -12,7 +12,14 @@ public class PlayerMovment : MonoBehaviour
 
     private void Update()
     {
+        float horizontalinput = Input.GetAxis("Horizontal");
         body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+
+        //Flip player when moving left-right//
+        if (horizontalinput < 0.01f)
+            transform.localScale = Vector3.one;
+        else if (horizontalinput > -0.01f)
+            transform.localScale = new Vector3(-1, 1, 1);
 
         if (Input.GetKey(KeyCode.Space))
             body.velocity = new Vector2(body.velocity.x, speed);
