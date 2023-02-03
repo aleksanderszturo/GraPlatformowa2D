6597b0d17fb3e9db71f4f0298ffeea92d0e5f843
aleksanderszturo <aleksanderszturo@gmail.com>
2023-02-03 14:44:03 +0100

    Acid

--- Assets/Scripts/Enemies/Enemy_Damage.cs
+++ Assets/Scripts/Enemies/Enemy_Damage.cs
@@ -1,18 +1,12 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy_ : MonoBehaviour
+public class Enemy_Damage : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private float damage;
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.tag == "Player")
+            collision.GetComponent<Health>().TakeDamage(damage);
     }
 }
